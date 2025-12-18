6a27df71812339c6cd52a6c27a32bc4c2166f431
John Lukach <john@lukach.net>
2025-12-18 03:58:37 +0000

    api

--- api/api_use1.py
+++ api/api_use1.py
@@ -1,20 +1,14 @@
 from aws_cdk import (
-    Duration,
     RemovalPolicy,
-    Size,
     Stack,
     aws_apigatewayv2 as _api,
     aws_apigatewayv2_integrations as _integrations,
     aws_certificatemanager as _acm,
-    aws_events as _events,
-    aws_events_targets as _targets,
     aws_iam as _iam,
     aws_lambda as _lambda,
     aws_logs as _logs,
     aws_route53 as _route53,
     aws_route53_targets as _r53targets,
-    aws_s3 as _s3,
-    aws_s3_deployment as _deployment,
     aws_ssm as _ssm
 )
 
@@ -73,4 +67,111 @@ class ApiUse1(Stack):
             parameter_name = '/route53/apilukachio',
             string_value = hostzone.hosted_zone_id,
             tier = _ssm.ParameterTier.STANDARD
+        )
+
+    ### ACM CERTIFICATE ###
+
+        acm = _acm.Certificate(
+            self, 'acm',
+            domain_name = 'use1.api.lukach.io',
+            subject_alternative_names = [
+                'api.lukach.io'
+            ],
+            validation = _acm.CertificateValidation.from_dns(hostzone)
+        )
+
+    ### DOMAIN NAME ###
+
+        domain = _api.DomainName(
+            self, 'domain',
+            domain_name = 'use1.api.lukach.io',
+            certificate = acm,
+            endpoint_type = _api.EndpointType.REGIONAL,
+            ip_address_type = _api.IpAddressType.DUAL_STACK
+        )
+
+    ### API LOG ROLE ###
+
+        apirole = _iam.Role(
+            self, 'apirole', 
+            assumed_by = _iam.ServicePrincipal(
+                'apigateway.amazonaws.com'
+            )
+        )
+
+        apirole.add_managed_policy(
+            _iam.ManagedPolicy.from_aws_managed_policy_name(
+                'service-role/AmazonAPIGatewayPushToCloudWatchLogs'
+            )
+        )
+
+        apilogs = _logs.LogGroup(
+            self, 'apilogs',
+            log_group_name = '/aws/apigateway/apilukachio',
+            retention = _logs.RetentionDays.THIRTEEN_MONTHS,
+            removal_policy = RemovalPolicy.DESTROY
+        )
+
+    ### API GATEWAY ###
+
+        api = _api.HttpApi(
+            self, 'api',
+            api_name = 'api.lukach.io',
+            description = 'use1.api.lukach.io',
+            default_domain_mapping = _api.DomainMappingOptions(
+                domain_name = domain
+            ),
+            ip_address_type = _api.IpAddressType.DUAL_STACK
+        )
+
+    ### GEOLITE FUNCTION ###
+
+        geoliteaccount = _ssm.StringParameter.from_string_parameter_attributes(
+            self, 'geoliteaccount',
+            parameter_name = '/account/geolite'
+        )
+
+        geolite = _lambda.Function.from_function_attributes(
+            self, 'geolite',
+            function_arn = 'arn:aws:lambda:us-east-1:'+geoliteaccount.string_value+':function:search',
+            same_environment = False,
+            skip_permissions = True
+        )
+
+        geoliteintegration = _integrations.HttpLambdaIntegration(
+            'geoliteintegration', geolite
+        )
+
+        api.add_routes(
+            path = '/geo/geolite2',
+            methods = [
+                _api.HttpMethod.GET
+            ],
+            integration = geoliteintegration
+        )
+
+    ### DNS RECORDS
+
+        ipv4dns = _route53.ARecord(
+            self, 'ipv4dns',
+            zone = hostzone,
+            record_name = 'use1.api.lukach.io',
+            target = _route53.RecordTarget.from_alias(
+                _r53targets.ApiGatewayv2DomainProperties(
+                    domain.regional_domain_name,
+                    domain.regional_hosted_zone_id
+                )
+            )
+        )
+
+        ipv6dns = _route53.AaaaRecord(
+            self, 'ipv6dns',
+            zone = hostzone,
+            record_name = 'use1.api.lukach.io',
+            target = _route53.RecordTarget.from_alias(
+                _r53targets.ApiGatewayv2DomainProperties(
+                    domain.regional_domain_name,
+                    domain.regional_hosted_zone_id
+                )
+            )
         )
